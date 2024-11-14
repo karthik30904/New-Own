8bb96c0f5086d7f9c21a613ea881f3bcafa6b373
karthik30904 <karthiksuresh309@gmail.com>
2024-11-14 19:01:28 +0530

    hyphennn

---
+++ command.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+class command{
+    public static void main(String args[]){
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[i]+"ram,dharz");
+        }
+    }
+}
---
+++ kindly1.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+class kindly1
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to learning!");
+	}
+}
